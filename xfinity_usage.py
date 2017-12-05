c669d2f1040bbe9bce3935aadb302c8f3be11bfe
Jason Antman <jason@jasonantman.com>
2017-12-04 21:00:49 -0500

    fixes #8 - update for redesign that removed ng-if=device.usage element

--- xfinity_usage.py
+++ xfinity_usage.py
@@ -50,6 +50,9 @@ The latest version of this script can be found at:
 CHANGELOG (see VERSION constant for version number)
 ---------------------------------------------------
 
+1.2.0 2017-12-04 Jason Antman <jason@jasonantman.com>
+  - Update for redesign that removed ``ng-if="device.usage"`` element.
+
 1.1.0 2017-11-30 Jason Antman <jason@jasonantman.com>
   - Merge PR #6 from ericzinnikas to handle reporting used amount even if it
     is over data cap.
@@ -112,7 +115,7 @@ except ImportError:
     sys.stderr.write("Error importing selenium - 'pip install selenium'\n")
     raise SystemExit(1)
 
-VERSION = '1.0.0'
+VERSION = '1.2.0'
 
 FORMAT = "[%(asctime)s %(levelname)s] %(message)s"
 logging.basicConfig(level=logging.WARNING, format=FORMAT)
@@ -284,26 +287,36 @@ class XfinityUsage(object):
                 '//*'
                 '[@ng-bind-html="usage.details.userMessage.monthlyUsageState"]'
             )
-            device = self.browser.find_element_by_xpath(
-                '//*[@ng-if="device.usage"]'
-            )
             logger.debug('Found monthly usage divs')
         except Exception:
-            logger.critical('Unable to find monthly usage divs on page',
+            logger.critical('Unable to find monthly usage div on page',
                             exc_info=True)
             self.error_screenshot()
-            raise RuntimeError('Unable to find monthly usage divs.')
+            raise RuntimeError('Unable to find monthly usage div.')
         logger.debug('Usage meter text: %s', meter.text)
-        logger.debug('Device text: %s', device.text)
+        try:
+            used_xpath = '//div[@usage-data="usage.details.usageData"]' \
+                         '//tr[last()]/td[1]'
+            self.wait_by(By.XPATH, used_xpath)
+            used_td = self.browser.find_element_by_xpath(used_xpath)
+            logger.debug('Found current Total Monthly Usage table cell')
+            used_value = used_td.get_attribute('innerHTML')
+        except Exception:
+            logger.critical('Unable to find current Total Monthly Usage '
+                            'table cell', exc_info=True)
+            self.error_screenshot()
+            raise RuntimeError('Unable to find current Total Monthly Usage '
+                               'table cell.')
+        logger.debug('Montly Usage TD: %s', used_value)
         m = re.search(
             r'(\d+)([A-Za-z]+) remaining of (\d+)([A-Za-z]+) monthly plan',
             meter.text
         )
         if m is None:
             raise RuntimeError('Cannot parse string: %s' % meter.text)
-        d = re.search(r'Used (\d+)([A-Za-z]+)', device.text)
+        d = re.search(r'(\d+)\s*([A-Za-z]+)', used_value)
         if d is None:
-            raise RuntimeError('Cannot parse string: %s' % device.text)
+            raise RuntimeError('Cannot parse string: %s' % used_value)
         used = float(d.group(1))
         used_unit = d.group(2)
         total = float(m.group(3))
